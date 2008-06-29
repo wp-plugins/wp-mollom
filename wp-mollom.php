49cb927d130111aee1677eda99b153794ffa7bcd
Netsensei <Netsensei@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-06-29 16:14:10 +0000

    update CSS Mollom Captcha
    git-svn-id: https://plugins.svn.wordpress.org/wp-mollom/trunk@52378 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp-mollom.php
+++ wp-mollom.php
@@ -923,12 +923,65 @@ function _mollom_show_captcha($message = '', $mollom_comment = array()) {
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<title>WordPress &raquo; Mollom CAPTCHA test</title>
 	<link rel="stylesheet" href="<?php get_bloginfo('siteurl'); ?>/wp-admin/css/install.css" type="text/css" />
+	<style media="screen" type="text/css">
+		html { background: #f1f1f1; }
+		
+		body {
+			background: #fff;
+			color: #333;
+			font-family: "Lucida Grande", "Lucida Sans Unicode", Tahoma, Verdana, sans-serif;
+			margin: 2em auto 0 auto;
+			width: 700px;
+			padding: 1em 2em;
+			-webkit-border-radius: 12px;
+			font-size: 62.5%;
+		}
+
+		a { color: #2583ad; text-decoration: none; }
+
+		a:hover { color: #d54e21; }
+
+		h2 { font-size: 16px; }
+
+		p {
+			padding-bottom: 2px;
+			font-size: 1.3em;
+			line-height: 1.8em;
+		}	
+
+		#logo { margin: 6px 0 14px 0px; border-bottom: none;}
+
+		h1 {
+			border-bottom: 1px solid #dadada;
+			clear: both;
+			color: #666666;
+			font: 24px Georgia, "Times New Roman", Times, serif;
+			margin: 5px 0 0 -4px;
+			padding: 0;
+			padding-bottom: 7px;
+		}
+
+		#error-page {
+			margin-top: 50px;
+		}
+
+		#error-page p {
+			font-size: 14px;
+			line-height: 1.6em;
+		}
+		
+		#error-page p.message {
+			border: 1px solid #d91f1f;
+			background: #f88b8b;
+			padding: 0 0 0 5px;
+		}
+      </style>
 </head>
 <body id="error-page">
 <h1>Mollom CAPTCHA</h1>
 <p><?php _e('This blog is protected by <a href=\"http://mollom.com\">Mollom</a> against spam. Mollom is unsure wether your comment was spam or not. Please complete this form by typing the text in the image in the input box. Additionally, you can also listen to a spoken version of the text.'); ?></p>
 <?php if ($message != '') { ?>
-	<p style="border: 1px solid #d91f1f; background: #f88b8b; padding: 0 0 0 5px;"><?php _e($message); ?></p>
+	<p class="message"><?php _e($message); ?></p>
 <?php } ?>
 <form action="wp-comments-post.php" method="post">
 	<p><label><strong><?php _e('Image Captcha'); ?></strong></label></p>
