1a3230a80301449642dbd22669fdc7d7a3fbd072
Netsensei <Netsensei@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-12-25 22:32:00 +0000

    git-svn-id: https://plugins.svn.wordpress.org/wp-mollom/trunk@82445 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- uninstall.php
+++ uninstall.php
@@ -3,7 +3,6 @@
 Uninstall logic when WP-Mollom is deleted  through the admin panel(>= WP 2.7)
 */
 
-
 // do not run unless within the WP plugin flow
 if( (!defined("ABSPATH")) && (!defined("WP_UNINSTALL_PLUGIN")) ) {
 	define( 'MOLLOM_I8N', 'wp-mollom' );	wp_die(__('The uninstall is not being executed from plugins.php. Halting.', MOLLOM_I8N));
